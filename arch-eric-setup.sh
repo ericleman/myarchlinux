22f7c57cc2b63c8fad85d685b5590a1cfec92915
ericleman <eric.leman@gmail.com>
2017-11-23 14:17:36 +0100

    Update arch-eric-setup.sh

--- arch-eric-setup.sh
+++ arch-eric-setup.sh
@@ -294,27 +294,11 @@ configure() {
   chown eric /home/eric/.xprofile
   chmod 644 /home/eric/.xprofile
 
-  cat > /home/eric/gsettings.sh <<EOF  
-gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
-gsettings set org.gnome.desktop.interface icon-theme 'Arc'
-gsettings set org.gnome.shell.extensions.user-theme name 'Arc-Dark'
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'chromium.desktop', 'sublime-text.desktop']"
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'fr+mac')]"
-gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-gsettings set org.gnome.desktop.interface clock-show-date true
-gsettings set org.gnome.desktop.interface clock-show-seconds true
-gsettings set org.gnome.desktop.session idle-delay 0
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>t'
-gsettings set org.gnome.shell enabled-extensions "['workspace-indicator@gnome-shell-extensions.gcampax.github.com','user-theme@gnome-shell-extensions.gcampax.github.com','alternate-tab@gnome-shell-extensions.gcampax.github.com', 'arch-update@RaphaelRochet', 'TopIcons@phocean.net', 'EasyScreenCast@iacopodeenosee.gmail.com']"
-gsettings set org.gnome.shell.extensions.topicons tray-pos 'right'
-EOF
+  cp /scripts/gsettings.sh /home/eric/gsettings.sh
   chown eric /home/eric/gsettings.sh
   chmod 700 /home/eric/gsettings.sh
+  su eric -c "/home/eric/gsettings.sh"
 
-  # su eric -c "/home/eric/gsettings.sh"
   rm /arch-eric-setup.sh
 }
 
